392bb49b5c19e59facc8782ec766254e3031d970
hassehulabeck <hansbudandersson@gmail.com>
2020-08-26 10:19:57 +0200

    Added emptyCart

--- index.js
+++ index.js
@@ -29,11 +29,33 @@ function putInCart(productID, amount) {
     // Kolla hur många det finns, och ta antingen så många kunden vill ha eller så många som finns.
     amount = (amount <= products[index].amount) ? amount : products[index].amount
 
-    /* Om jag bara refererar till ett objekt, så får jag bara en referens till det.
+    /* Om jag refererar till ett objekt, så får jag bara en referens till det.
+
+        exempel: cart.push(products[index])
+
+        Vilket gör att både cart och products kommer att ha en referens till exakt samma objekt.
+
     Med Object.assign kan jag "kopiera" över egenskaperna, och skapa ett fristående objekt. */
     let length = cart.push(Object.assign({}, products[index]))
 
     // Justera antalet (amount) i såväl products som i cart.
     cart[length - 1].amount = amount
     products[index].amount -= amount
+}
+
+function emptyCart() {
+    /* Att bara tömma varukorgen räcker inte. 
+    Vi måste återbörda innehållet till products.
+    */
+
+    cart.forEach((product) => {
+        let p = products.find((prod) => {
+            return prod.id == product.id
+        })
+        p.amount += product.amount
+    })
+
+    // Nu kan vi tömma varukorgen
+    cart = [];
+
 }
